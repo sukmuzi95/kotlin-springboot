afea0da09cb87d464ac02ff2ff2d2696976c9e86
sukmuzi <sukmuzi@naver.com>
2022-06-20 16:16:00 +0900

    init

---
+++ src/test/kotlin/com/example/kotlinspringbootboard/KotlinSpringbootBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
